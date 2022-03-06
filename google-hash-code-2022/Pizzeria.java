1a8a1939a8a4f315e40d41311cd84a76e8f1d917
Aastha Kumari <aasthak108@gmail.com>
2022-03-06 15:59:28 +0530

    update

--- google-hash-code-2022/Pizzeria.java
+++ google-hash-code-2022/Pizzeria.java
@@ -5,14 +5,25 @@ class Pizzeria
 	public static void main(String args[])
 	{
 		// Pizza Selection Program
+		int count = 0;
 		int i=0;
-		int j;
-		String[] array = new String[10];
-		System.out.println("Enter Like ingridients");
-		Scanner s = new Scanner(System.in);
-		array[i] = s.nextLine();
-		for(String likeArray : array.split(" "));	
-		System.out.println(likeArray);
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no. of potential clients");
+		int clientNumber = sc.nextInt();
+		String[] mainLikeArray = new String[20];
+		for(int j=0;j<=clientNumber*2;j++)
+		{
+			System.out.println("Enter Like ingridients");
+			String ingridients = sc.nextLine();
+			String[] likeArray = ingridients.split("\\s+");
+			System.out.println("Liked Ingridients are");
+			for(i=count;i<likeArray.length;i++)
+			{
+				System.out.print(" " +likeArray[i]);
+				mainLikeArray[count] = likeArray[i];
+				count++;
+			}
+		}
+		System.out.println("Main array is" +mainLikeArray);		
 		
 }
