d7c4d6cb15fddcf8516596dde3051d1e72704982
Aastha Kumari <aasthak108@gmail.com>
2022-06-26 23:23:42 +0530

    update

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,10 +1,9 @@
+
 class BinarySearch
 {
-	public int doBinarySearch(int arr[], int key)
+	public int doBinarySearch(int arr[], int key,int l,int r)
 	{
-		int l = 0;
-		int r = arr.length - 1;
-		(l<=r)
+		while(l<=r)
 		{
 			int mid= (l+r)/2;
 			if(key == arr[mid])
@@ -25,9 +24,11 @@ class BinarySearch
 		
 	public static void main(String args[])
 		{
-			int arr[]={1,3,7,8,15,56,90};
-			int valueToBeSearched = 0;
-			int indexOfNumberFound = new BinarySearch().doBinarySearch(arr, valueToBeSearched);
+			int arr[]={0,0,0,1,1,1,1};
+			int key= 1;
+			int l = 0;
+			int r = arr.length;
+			int indexOfNumberFound = new BinarySearch().doBinarySearch(arr, key,l,r);
 			if(indexOfNumberFound != -1)
 			{
 				System.out.println(" The searched key is at index " + indexOfNumberFound);
