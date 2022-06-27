53b65bfe0f17ce9c372c39e094ad1e45636a5f49
Aastha Kumari <aasthak108@gmail.com>
2022-06-28 00:25:00 +0530

    update

--- Array/PrefixSum.java
+++ Array/PrefixSum.java
@@ -3,21 +3,21 @@ class PrefixSum
  public int[] prefixadd(int a[])	
  {
 	int sum[]= new int[4]; 
-   sum[0]=a[0];
-   int i;
-  for(i=1;i<a.length;i++)
+    sum[0]=a[0];
+    int i;
+    for(i=1;i<a.length;i++)
 	{
-	 sum[i] =sum[i-1]+a[i];
+		sum[i] =sum[i-1]+a[i];
 	}
-  return sum;
+	return sum;
  }
  public static void main(String args[])
  {
- int a[]={10,20,5,15};
- int arr[]=new PrefixSum().prefixadd(a);
- for(int i=0;i<a.length;i++)
- {
-  System.out.print(arr[i]+ " ");
- }
+	int a[]={10,20,5,15};
+	int arr[]=new PrefixSum().prefixadd(a);
+	for(int i=0;i<a.length;i++)
+	{
+		System.out.print(arr[i]+ " ");
+	}
 }
 }
