a6b6dd99a437b10b8e3a2a0954bf9b68616ab3da
Aastha Kumari <aasthak108@gmail.com>
2022-07-12 23:19:45 +0530

    update

--- Array/PrintDistinctElements.java
+++ Array/PrintDistinctElements.java
@@ -3,6 +3,19 @@ class PrintDistinctElements
 	public static void main(String args[])
 	{
 		int arr[]={12,10,9,45,2,10,10,45};
+		int count =0;
+		int c=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=0;j<arr.length;j++)
+			{
+			if(arr[i] == arr[j])
+			{
+				count++;
+			}
+			}
+		}
+		count = c;
 		int arr1[] = new PrintDistinctElements().printDistinctElements(arr);
 		for(int i=0;i<arr.length;i++)
 		{
@@ -12,6 +25,7 @@ class PrintDistinctElements
 	
 	public static int[] printDistinctElements(int arr[])
 	{
+		int a[] = new int[arr.length - count];
 		for(int i=0;i<arr.length;i++)
 		{
 			for(int j =1;j<arr.length;j++)
