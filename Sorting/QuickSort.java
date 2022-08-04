a0f35976129b2bcd53e2ab9aa1285cadc069a5bf
Aastha Kumari <aasthak108@gmail.com>
2022-08-04 21:30:08 +0530

    update

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,32 +1,46 @@
 class QuickSort{
-public int[] quickSort(int arr[],int h,int l)
+public static int partition(int arr[],int low,int high)
 {
-	int i=l-1;
-	for(int j=0;j<arr.length;j++)
+	int i = low -1;
+	int pivot = arr[high];
+	for(int j = low;j<high;j++)
 	{
-		if(arr[j]<=arr[h])
+		if(arr[j]<=pivot)
 		{
 			i++;
-			int temp=arr[j];
-			arr[j]=arr[i];
-			arr[i]=temp;
+			int temp = arr[i];
+			arr[i]=arr[j];
+			arr[j] = temp;
 		}
 	}
-		int temp=arr[h];
-		arr[h]=arr[i+1];
-		arr[i+1]= temp;
-		return arr;
+	int temp = arr[i+1];
+	arr[i+1]=arr[high];
+	arr[high] = temp;
+	return i+1;
+}
+void quickSort(int arr[],int low,int high)
+{
+	if(low<high)
+	{
+		int p = partition(arr,low,high);
+		quickSort(arr,low,p-1);
+		quickSort(arr,p+1,high);
+	}
+}
+void print(int arr[])
+{
+	for(int i=0;i<arr.length;i++)
+	{
+		System.out.println(""+arr[i]);
+	}
 }
 public static void main(String args[])
 {
-	int arr[]={10,80,30,90,40,50,70};
-	int h= arr.length-1;
-	int l= 0;
-	//pivot = 70
-	int SortedArr[]=new QuickSort().quickSort(arr,h,l);
-	for(int j=0;j<arr.length;j++)
-	{
-		System.out.println(SortedArr[j] + " is the required array");
-	}
+	int arr[] = {5,3,7,2,6,1};
+	int low = 0;
+	int high = arr.length-1;
+	QuickSort s = new QuickSort();
+	s.quickSort(arr,low,high);
+	s.print(arr);
 }
 }
