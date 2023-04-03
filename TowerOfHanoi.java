de6dbd1e4aeab806d435a7a8642dc3cadbb8a6a0
aastha <aasthak108@gmail.com>
2023-04-03 23:33:30 +0530

    update

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,20 +1,19 @@
-class TowerOfHanoi
-{
-	void towerOfHanoi(int n, String s, String d, String h)
-	{
-		if(n<=0)
-		{
-			return;
-		}
-		else
-		towerOfHanoi(n-1,s,h,d);
-		System.out.println("Disk"+n+"moved from"+s+"to"+d);
-		towerOfHanoi(n-1,h,d,s);
-	}
-	public static void main(String args[])
-	{
-		TowerOfHanoi t = new TowerOfHanoi();
-		t.towerOfHanoi(3,"Source","Destination","Helper");
-	}
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n,int A,int B,int C)
+    {
+        if(n<=0)
+        {
+            return;
+        }
+        towerOfHanoi(n-1,A,C,B);
+        System.out.println("the disc " + n + " is moved from " + A + " to " +C);
+        towerOfHanoi(n-1,B,A,C);
+    }
+    public static void main(String args[])
+    {
+        int n=5;
+        int A=1,B=2,C=3;
+        towerOfHanoi(n,A,B,C);
+    }
+
 }
-		
